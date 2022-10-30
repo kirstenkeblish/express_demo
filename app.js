a3adf07a5352123c911b0ffe067dbb74cb9bed0e
kirstenkeblish <kirstenkeblish@gmail.com>
2022-10-29 20:21:10 -0600

    nodemon

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello NodeJS!</h1>')
+  res.send('<h3>Kirsten Keblish - f51c944</h3>')
+  res.send('<p>Example app listening on port 3035</p>')
 })
 
 app.listen(port, () => {

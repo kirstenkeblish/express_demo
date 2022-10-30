f57910b7cb21bbba2113d502e4f4377ed5ef9702
kirstenkeblish <kirstenkeblish@gmail.com>
2022-10-29 20:41:11 -0600

    port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3035
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello NodeJS!</h1><h3>Kirsten Keblish - f51c944</h3><p>Example app listening on port 3035</p>')
